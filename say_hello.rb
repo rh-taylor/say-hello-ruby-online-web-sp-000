e9b4bdd7b2d2e07872f3ca3acaf2c480c0537601
Robyn Taylor <rhtaylor0715@gmail.com>
2021-02-26 20:08:19 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,4 @@
 # Build your say_hello method here
-def say_hello(specific_name, generic_name = "Ruby Progammer")
+def say_hello(name = "Ruby Progammer")
   puts "Hello, #{specific_name}, #{generic_name} !"
 end
